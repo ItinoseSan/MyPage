13f9729496ca2bf0f8200df88f516d319f6d00b7
ItinoseSan <jkekun@gmail.com>
2017-08-13 22:51:26 +0900

    first

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'top/index', to: 'top#index'
-  root 'top/show',  to:'top#show'
+  get 'top/index', to: 'top#index'
+  get 'top/show',  to:'top#show'
 end
